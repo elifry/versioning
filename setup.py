87087fb5c28838d17f075d198c91f394cda6d084
Eli Fry <33290919+elifry@users.noreply.github.com>
2023-03-10 12:32:53 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="versioning",
+    version="0.0.1",
+    author="Eli Fry",
+    description="Format version numbers, build strings for various uses, classes for versioning schemes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
