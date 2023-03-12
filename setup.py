3a54a4db891644b732815fe3a3897ae084393533
Eli Fry <33290919+elifry@users.noreply.github.com>
2023-03-12 00:11:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import setuptools
-from versioning import __version__
 
 setuptools.setup(
     name="elifry_versioning",
-    version=__version__,
+    version="0.0.1",
     author="Eli Fry",
     description="Format version numbers, build strings for various uses, classes for versioning schemes",
     packages=setuptools.find_packages(),
