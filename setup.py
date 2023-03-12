a3b2b74d9b662d5924bcc97df761063545945f24
Eli Fry <33290919+elifry@users.noreply.github.com>
2023-03-12 01:16:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import setuptools
 
+# Get the long description from the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="elifry_versioning",
     version='0.0.1',
     author="Eli Fry",
     description="Format version numbers, build strings for various uses, classes for versioning schemes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
