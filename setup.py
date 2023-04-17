0f89e59d83affe601f219894e01cf06c0d24e7b9
Eli Fry <fryjah07@gmail.com>
2023-04-17 19:37:32 -0400

    Versioning ci

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="elifry_versioning",
-    version='0.0.1',
+    version='{version}',
     author="Eli Fry",
     description="Format version numbers, build strings for various uses, classes for versioning schemes",
     long_description=long_description,
